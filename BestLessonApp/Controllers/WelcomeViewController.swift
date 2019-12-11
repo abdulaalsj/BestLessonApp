7cf05ad910aa8010ff7ffbfde4bf549790121dfa
Summer Abdulaal <abdulaalsj@g.cofc.edu>
2019-12-11 12:22:40 -0500

    Main Story board View controller  with  UI label

---
+++ BestLessonApp/Controllers/WelcomeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WelcomeViewController.swift
+//  BestLessonApp
+//
+//  Created by Sumer Abdulaal on 12/11/19.
+//  Copyright © 2019 Sumer Abdulaal. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       // titleLabel.text = "Learning Beez"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
